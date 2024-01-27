755d5618bacc039fbbc4e975a4f5abda8ab33de3
rajmalpure <malpure.raj@kalvium.community>
2024-01-27 10:55:22 +0530

    added js

--- app.js
+++ app.js
@@ -120,10 +120,23 @@ function updatePrice() {
   finalCents = totalPriceInCents % 100;
 }
 
+//WhatsApp Integration
+var whatsappLink = "https://api.whatsapp.com/send?phone=917796652577&text=Order%20details:"
+
+function updateWhatsappLink(){
+  for(let index = 0; index < items.length ; index++){
+    if(items[index].quantity != 0){
+      whatsappLink += "%0A" + items[index].name + "%20" + items[index].quantity;
+    }
+  }
+  whatsappLink += "%0A" + "Total%20Price:%20$" + finalDollars + "%20" + finalCents + "c";
+}
 
 cartButton.onclick = () => {
   updatePrice();
-
+  //Calling updateWhatsappLink()
+  updateWhatsappLink()
+  window.open(whatsappLink, "_blank");
 
   for (let index = 0; index < items.length; index++) {
     if (items[index].quantity != 0) {
